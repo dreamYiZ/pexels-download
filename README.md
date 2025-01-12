6830df0f7b64bc01423dc5ec7d2b4388831f536f
Rentoo <dreamYiZ@outlook.com>
2025-01-12 16:49:43 +0800

    -

--- README.md
+++ README.md
@@ -1,26 +1,71 @@
-# download pexels photo
-
-下载pexels图片文件
 
 
-## setup
+---
 
-put PEXEL_API_KEY in environment variables;
+# Download Pexels Photo
 
+下载Pexels图片文件
 
-## install 
+## Setup
+
+1. 将 `PEXEL_API_KEY` 放入环境变量中：
+   ```bash
+   export PEXEL_API_KEY=your_api_key_here
+   ```
+
+## Install
+
+首先，你需要安装依赖：
 ```bash
-npm i 
+npm i
 ```
 
+## Run
 
-## run
+你可以使用以下命令运行程序并下载指定关键词的Pexels图片：
+
 ```bash
 npm run dev cat
 ```
-
-or
-
+或
 ```bash
 npm run dev [keyword]
 ```
+将 `[keyword]` 替换为你想要搜索的关键字。
+
+## Configuration
+
+你可以在`config`文件中写入配置。需要注意的是，环境变量与命令行参数的优先级更高。
+
+### 配置示例
+
+在`config`文件中，你可以添加如下配置：
+```json
+{
+  "api_key": "your_api_key_here",
+  "default_keyword": "cat",
+  "output_directory": "./downloads"
+}
+```
+
+### 运行示例
+
+1. 使用环境变量设置API密钥并运行程序：
+   ```bash
+   export PEXEL_API_KEY=your_api_key_here
+   npm run dev dog
+   ```
+
+2. 使用命令行参数设置关键词：
+   ```bash
+   npm run dev bird
+   ```
+
+3. 使用配置文件设置默认关键词并运行程序：
+   ```bash
+   npm run dev
+   ```
+
+---
+
+希望这些改进能够帮助你更好地使用这个脚本。如果你有任何问题或需要进一步的帮助，随时告诉我！
--- index.js
+++ index.js
@@ -1,109 +1,81 @@
 const fs = require('fs');
 const request = require('request');
 const path = require('path');
+const config = require('./config.json'); // 读取 config.json
 
-const PEXEL_API_KEY = process.env.PEXEL_API_KEY;
+const PEXEL_API_KEY = process.env.PEXEL_API_KEY || config.PEXEL_API_KEY;
 console.log('[PEXEL_API_KEY] --> ', PEXEL_API_KEY);
 
 const DOWNLOAD_PATH = 'pexels-download/';
-
-const PER_PAGE = 5;
-
-
+const PER_PAGE = process.env.PER_PAGE || config.PER_PAGE || 5; // 优先级: env > config > 默认值
+let keyword = process.argv[2] || process.env.KEYWORD || config.keyword || 'cat'; // 优先级: 命令行参数 > env > config > 默认值
 
 if (!fs.existsSync(DOWNLOAD_PATH)) {
-    fs.mkdirSync(DOWNLOAD_PATH);
+  fs.mkdirSync(DOWNLOAD_PATH);
 }
 
 const BASE_API_PATH = 'https://api.pexels.com/v1/';
-const API_SEARCH = BASE_API_PATH + 'search'
-
+const API_SEARCH = BASE_API_PATH + 'search';
 
 const baseRequest = ({ url, method = "GET" }) => {
-    return fetch(url, {
-        method: method,
-        headers: {
-            'Authorization': PEXEL_API_KEY,
-        },
-    })
+  return fetch(url, {
+    method: method,
+    headers: {
+      'Authorization': PEXEL_API_KEY,
+    },
+  });
 }
 
-
-const searchPhoto = (
-    options
-) => {
-    let queryString = '';
-    Object.keys(options).forEach(key => {
-        if (queryString) {
-            queryString = `${queryString}&`
-        } else {
-            queryString = '?'
-        }
-
-        queryString = `${queryString}${key}=${options[key]}`
-
-    })
-    return baseRequest({ url: API_SEARCH + queryString })
+const searchPhoto = (options) => {
+  let queryString = '';
+  Object.keys(options).forEach(key => {
+    if (queryString) {
+      queryString = `${queryString}&`;
+    } else {
+      queryString = '?';
+    }
+    queryString = `${queryString}${key}=${options[key]}`;
+  });
+  return baseRequest({ url: API_SEARCH + queryString });
 }
-
-
-
-
 
 const download = function (uri, filename, callback) {
-    request.head(uri, function (err, res, body) {
-        console.log('content-type:', res.headers['content-type']);
-        console.log('content-length:', res.headers['content-length']);
-
-        request(uri).pipe(fs.createWriteStream(DOWNLOAD_PATH + filename)).on('close', callback);
-    });
+  request.head(uri, function (err, res, body) {
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    request(uri).pipe(fs.createWriteStream(DOWNLOAD_PATH + filename)).on('close', callback);
+  });
 };
 
-
 const getFileNameByUrl = (url) => {
-    return url.split('/').pop()
+  return url.split('/').pop();
 }
 
 const emptyDownloadFolder = () => {
-    fs.readdir(DOWNLOAD_PATH, (err, files) => {
+  fs.readdir(DOWNLOAD_PATH, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      fs.unlink(path.join(DOWNLOAD_PATH, file), (err) => {
         if (err) throw err;
-
-        for (const file of files) {
-            fs.unlink(path.join(DOWNLOAD_PATH, file), (err) => {
-                if (err) throw err;
-            });
-        }
-    });
+      });
+    }
+  });
 }
 
 const startSearchAndDownload = () => {
-    let keyword = process.argv[2] || 'cat';
-
-    searchPhoto({
-        query: keyword,
-        per_page: PER_PAGE,
-    }).then(res => {
-        return res.json();
-    }).then((response) => {
-        // console.log('response', response)
-        response.photos.forEach(photo => {
-            console.log(photo.src.original)
-
-            download(photo.src.original, getFileNameByUrl(photo.src.original), () => {
-
-            })
-        })
+  searchPhoto({ query: keyword, per_page: PER_PAGE }).then(res => {
+    return res.json();
+  }).then((response) => {
+    response.photos.forEach(photo => {
+      console.log(photo.src.original);
+      download(photo.src.original, getFileNameByUrl(photo.src.original), () => { });
     });
+  });
 }
 
-
 const main = () => {
-
-
-    emptyDownloadFolder();
-
-    startSearchAndDownload();
-
+  emptyDownloadFolder();
+  startSearchAndDownload();
 }
 
 main();
